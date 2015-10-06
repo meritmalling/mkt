548d272de2216d656da9a003baf33491c96e28e3
merittrompeter <mktrompeter@gmail.com>
2015-10-06 13:37:28 -0700

    remove carousel

--- script.js
+++ script.js
@@ -1,15 +1,78 @@
 $(document).ready(function(){
     console.log('Running')
 
+    $('.tlt').textillate({
+      selector: '.texts',
+      loop: false,
+      minDisplayTime: 2000,
+      initialDelay: 0,
+      autoStart: true
+    });
+
+    $('#about').on('click', function(){
+        $('.about').removeClass('hide').addClass('show');
+        $('.portfolio').removeClass('show').addClass('hide');
+        $('.resume').removeClass('show').addClass('hide');
+        $('#bottom').css('height', 800)
+    })
+
+    $('#portfolio').on('click', function(){
+        $('.portfolio').removeClass('hide').addClass('show');
+        $('.about').removeClass('show').addClass('hide');
+        $('.resume').removeClass('show').addClass('hide');
+        $('#bottom').css('height', 2200)
+    })
+
+    $('#resume').on('click', function(){
+        $('.resume').removeClass('hide').addClass('show');
+        $('.portfolio').removeClass('show').addClass('hide');
+        $('.about').removeClass('show').addClass('hide');
+        $('#bottom').css('height', 1200)
+    })
+    $('#home').on('click', function(){
+        location.reload();
+    })
+
+    // $('.tag').on('click', function(event){
+    //     console.log('Clicked Tag')
+
+    //     event.preventDefault();
+
+    //     var currentTag = $('.tagShow');
+    //     // console.log('CURRENT',currentTag)
+    //     var nextTag = currentTag.next();
+    //     // console.log('NEXT',nextTag)
+
+    //     if (nextTag.length === 0){
+    //         nextTag = $('.tag h1').first()
+    //     }
+
+    //     currentTag.removeClass('tagShow').addClass('tagHide').css('z-index', -10)
+    //     nextTag.addClass('tagShow').removeClass('tagHide').css('z-index', 20);
+    //     $('.tag').not([currentTag, nextTag]).css('z-index', 1)
+
+    //     console.log('CC Clicked')
+
+    // })
+
     var count = 1;
 
     $('html').on('click', function(){
         count ++;
+        var isGray = false;
         if (count % 2 == 0){
             $('html').addClass('gray')
+            isGray = true;
+
         } else {
             $('html').removeClass('gray')
+            isGray = false
         };
+    //     if (isGray === false){
+    //         $('html').css('background-color', '#0A2531')
+    //     } else if (isGray === true){
+    //         $('html').addClass('gray')
+    // }
     });
 
     $('#next').on('click', function(event){
@@ -33,15 +96,17 @@ $(document).ready(function(){
 
         event.preventDefault();
 
-        var currentImage = $('.show');
+        var currentImage = $('.showImg');
         var nextImage = currentImage.prev();
 
         if (nextImage.length === 0){
             nextImage = $('.work img').last()
         }
 
-        currentImage.removeClass('show').addClass('hide').css('z-index', -10)
-        nextImage.addClass('show').removeClass('hide').css('z-index', 20);
+        currentImage.removeClass('showImg').addClass('hideImg').css('z-index', -10)
+        nextImage.addClass('showImg').removeClass('hideImg').css('z-index', 20);
         $('.carousel').not([currentImage, nextImage]).css('z-index', 1)
     });
+
+
 });
