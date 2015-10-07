d383818f45c10817eb72c32d4cee42a7b3d0bf99
merittrompeter <mktrompeter@gmail.com>
2015-10-07 10:55:32 -0700

    about

--- script.js
+++ script.js
@@ -4,61 +4,34 @@ $(document).ready(function(){
     $("#bottom").height($(document).height() - $('#top').height());
 
     $('.tlt').textillate({
-      selector: '.texts',
-      loop: true,
-      minDisplayTime: 2000,
-      initialDelay: 0,
-      autoStart: true
+        selector: '.texts',
+        loop: true,
+        minDisplayTime: 2000,
+        initialDelay: 0,
+        autoStart: true
     });
 
     $('#about').on('click', function(){
         $('.about').removeClass('hide').addClass('show');
         $('.portfolio').removeClass('show').addClass('hide');
         $('.resume').removeClass('show').addClass('hide');
-        $('#contact').css({ 'position': 'absolute', 'bottom': 0, 'left': 0, 'right': 0 })
-        // $('#bottom').css('height', 800)
     })
 
     $('#portfolio').on('click', function(){
         $('.portfolio').removeClass('hide').addClass('show');
         $('.about').removeClass('show').addClass('hide');
         $('.resume').removeClass('show').addClass('hide');
-        $('#contact').css({ 'padding': '10px', 'position': 'relative', 'bottom': 'auto', 'left': 'auto', 'right': 'auto' })
-        // $('#bottom').css('height', 2200)
     })
 
     $('#resume').on('click', function(){
         $('.resume').removeClass('hide').addClass('show');
         $('.portfolio').removeClass('show').addClass('hide');
         $('.about').removeClass('show').addClass('hide');
-        $('#contact').css({ 'padding': '10px', 'position': 'relative', 'bottom': 'auto', 'left': 'auto', 'right': 'auto' })
-        // $('#bottom').css('height', 1200)
     })
+
     $('#home').on('click', function(){
         location.reload();
     })
-
-    // $('.tag').on('click', function(event){
-    //     console.log('Clicked Tag')
-
-    //     event.preventDefault();
-
-    //     var currentTag = $('.tagShow');
-    //     // console.log('CURRENT',currentTag)
-    //     var nextTag = currentTag.next();
-    //     // console.log('NEXT',nextTag)
-
-    //     if (nextTag.length === 0){
-    //         nextTag = $('.tag h1').first()
-    //     }
-
-    //     currentTag.removeClass('tagShow').addClass('tagHide').css('z-index', -10)
-    //     nextTag.addClass('tagShow').removeClass('tagHide').css('z-index', 20);
-    //     $('.tag').not([currentTag, nextTag]).css('z-index', 1)
-
-    //     console.log('CC Clicked')
-
-    // })
 
     var count = 1;
     $('html').on('click', function(){
