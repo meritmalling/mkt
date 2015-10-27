7c573e81e56ca58a325735691871cf965fefcb80
merittrompeter <mktrompeter@gmail.com>
2015-10-26 23:27:38 -0700

    syntax

--- script.js
+++ script.js
@@ -15,41 +15,41 @@ $(document).ready(function(){
         $('.about').removeClass('hide').addClass('show');
         $('.portfolio').removeClass('show').addClass('hide');
         $('.resume').removeClass('show').addClass('hide');
-    })
+    });
 
     $('#portfolio').on('click', function(){
         $('.portfolio').removeClass('hide').addClass('show');
         $('.about').removeClass('show').addClass('hide');
         $('.resume').removeClass('show').addClass('hide');
-    })
+    });
 
     $('#resume').on('click', function(){
         $('.resume').removeClass('hide').addClass('show');
         $('.portfolio').removeClass('show').addClass('hide');
         $('.about').removeClass('show').addClass('hide');
-    })
+    });
 
     $('#home').on('click', function(){
         location.reload();
-    })
+    });
 
     var count = 1;
     $('html').on('click', function(){
         count ++;
-        if (count % 2 == 0){
-            $('#top').css({'background-color':'black', 'color':'white'})
-            $('#bottom').css({'background-color':'white', 'color':'black'})
-            $('embed').addClass('gray')
-            $('#home').css({'border-color':'white'})
-            $('.tlt').css({'border-color':'black'})
+        if (count % 2 === 0){
+            $('#top').css({'background-color':'black', 'color':'white'});
+            $('#bottom').css({'background-color':'white', 'color':'black'});
+            $('embed').addClass('gray');
+            $('#home').css({'border-color':'white'});
+            $('.tlt').css({'border-color':'black'});
         } else {
-            $('#top').css({'background-color':'#FAC6BF', 'color':'#C28C57'})
-            $('#bottom').css({'background-color':'#0A2531', 'color':'#C28C57'})
-            $('embed').removeClass('gray')
-            $('#home').css({'border-color':'#C28C57'})
-            $('.tlt').css({'border-color':'#C28C57'})
+            $('#top').css({'background-color':'#FAC6BF', 'color':'#C28C57'});
+            $('#bottom').css({'background-color':'#0A2531', 'color':'#C28C57'});
+            $('embed').removeClass('gray');
+            $('#home').css({'border-color':'#C28C57'});
+            $('.tlt').css({'border-color':'#C28C57'});
 
-        };
+        }
     });
 
     // $('#next').on('click', function(event){
