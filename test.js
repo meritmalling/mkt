17fe43c29b5fc8037b84d2bc06f8d3599e24c86a
merittrompeter <mktrompeter@gmail.com>
2015-10-29 22:33:44 -0700

    redesign

---
+++ test.js
@@ -0,0 +1,72 @@
+$(document).ready(function(){
+    console.log('Running')
+
+    var scroll = function(){
+        $('html, body').animate({
+            scrollTop: $("#buffer").offset().top
+        }, 500);
+    }
+
+    $('.tlt').textillate({
+        selector: '.texts',
+        loop: true,
+        minDisplayTime: 2000,
+        initialDelay: 0,
+        autoStart: true
+    });
+
+    $('#about').on('click', function(){
+        $('.about').removeClass('hide').addClass('show');
+        $('.work').removeClass('show').addClass('hide');
+        $('.resume').removeClass('show').addClass('hide');
+        scroll();
+    });
+
+    $('#portfolio').on('click', function(){
+        $('.work').removeClass('hide').addClass('show');
+        $('.about').removeClass('show').addClass('hide');
+        $('.resume').removeClass('show').addClass('hide');
+        scroll();
+    });
+
+    $('#resume').on('click', function(){
+        $('.resume').removeClass('hide').addClass('show');
+        $('.work').removeClass('show').addClass('hide');
+        $('.about').removeClass('show').addClass('hide');
+        scroll();
+    });
+
+    $('#home').on('click', function(){
+        location.reload();
+    });
+
+    $('#sudokucircle').on('mouseover', function(){
+        $('.piece').hide()
+        $('#sudokutext').removeClass('hide').show()
+    });
+
+    $('#quickcircle').on('mouseover', function(){
+        $('.piece').hide()
+        $('#quicktext').removeClass('hide').show()
+    });
+
+    $('#myhivecircle').on('mouseover', function(){
+
+        $('.piece').hide()
+        $('#hivetext').removeClass('hide').show()
+    });
+
+    $('#mesacircle').on('mouseover', function(){
+        $('.piece').hide()
+        $('#mesatext').removeClass('hide').show()
+    });
+
+    $('#chasecircle').on('mouseover', function(){
+        $('.piece').hide()
+        $('#chasetext').removeClass('hide').show()
+    });
+
+
+
+
+});
