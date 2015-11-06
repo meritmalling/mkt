4263a2ec3164a6e88fb46343499f153ac7620643
merittrompeter <mktrompeter@gmail.com>
2015-11-05 16:48:20 -0800

    mobile

--- email.js
+++ email.js
@@ -8,8 +8,6 @@ var domain = 'gmail.com';
 var finalString = reverseStr(user) + '@' + domain
 var emailString = '<a href="mailto:' + finalString +'"><h2><i class="fa fa-envelope-square"></i></h2></a>'
 
-console.log('first',emailString)
-
 $(document).ready(function() {
     $('#contact').append(emailString)
 })
